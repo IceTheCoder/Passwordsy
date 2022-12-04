4b5be7de76b3bfd3d32a23fd32a31420a77b226d
IceTheDev2 <115871297+IceTheDev2@users.noreply.github.com>
2022-12-04 13:19:38 +0200

    Importing tkinter as tk instead of importing everything from the module

--- PasswordGenerator/Passwordsy.py
+++ PasswordGenerator/Passwordsy.py
@@ -1,8 +1,8 @@
 import secrets
 import string
-from tkinter import *
+import tkinter as tk
 
-root = Tk()
+root = tk.Tk()
 
 characters = string.hexdigits + string.punctuation
 error = "An error occured, try again with a whole number greater than 0"
@@ -12,30 +12,30 @@ root.maxsize(854, 240)
 root.grid_rowconfigure(7, weight = 1)
 root.grid_columnconfigure(0, weight = 1)
 root.resizable(0,0)
-root.iconphoto(False, PhotoImage(file="logo.png"))
+root.iconphoto(False, tk.PhotoImage(file="logo.png"))
 root.title("Passwordsy")
 
-Welcome = Label(root, text = "Passwordsy", font = 'Helvetica 24')
+Welcome = tk.Label(root, text = "Passwordsy", font = 'Helvetica 24')
 Welcome.grid(row = 1)
 Welcome.grid_rowconfigure(1, weight = 1)
 Welcome.grid_columnconfigure(1, weight = 1)
 
-Question = Label(root, text = "(up to 100) Number of characters:", font = 'Helvetica 12')
+Question = tk.Label(root, text = "(up to 100) Number of characters:", font = 'Helvetica 12')
 Question.grid(row = 3)
 Question.grid_rowconfigure(1, weight = 1)
 Question.grid_columnconfigure(1, weight = 1)
 
-Input = Entry(root, width = 10, borderwidth = 2)
+Input = tk.Entry(root, width = 10, borderwidth = 2)
 Input.grid(row = 4)
 Input.grid_rowconfigure(1, weight = 1)
 Input.grid_columnconfigure(1, weight = 1)
 
-Tip = Label(root, text = "CTRL + C to copy \nCTRL + V to paste", font = 'Helvetica 12')
+Tip = tk.Label(root, text = "CTRL + C to copy \nCTRL + V to paste", font = 'Helvetica 12')
 Tip.grid(row = 2)
 Tip.grid_rowconfigure(1, weight = 1)
 Tip.grid_columnconfigure(1, weight = 1)
 
-def Click():
+def Click() -> str:
     '''After clicking on the done button,
         it generates a password,
         and shows it on the screen.
@@ -50,7 +50,6 @@ def Click():
 
         password = ''.join(secrets.choice(characters) for i in range(lengthInt))
 
-        passwordLabel = Text(root, width=100, height = 1, borderwidth = 0, font = 'Consolas 11')
         passwordLabel.insert(1.0, password)
         passwordLabel.grid(row = 6, pady = 10) # Shows the password.
         passwordLabel.configure(state = "disabled") # Makes it uneditable.
@@ -59,7 +58,7 @@ def Click():
 
         password = ""
     except ValueError:
-        passwordLabel = Text(root, width=100, height = 1, borderwidth = 0, font = 'Consolas 11')
+        passwordLabel = tk.Text(root, width=100, height = 1, borderwidth = 0, font = 'Consolas 11')
         passwordLabel.insert(1.0, error)
         passwordLabel.grid(row = 6, pady = 10) # Shows the password.
         passwordLabel.configure(state = "disabled") # Makes it uneditable.
@@ -67,9 +66,9 @@ def Click():
         passwordLabel.grid_columnconfigure(1, weight = 1)
 
 
-done_btn = PhotoImage(file="doneButton.png")
+done_btn = tk.PhotoImage(file="doneButton.png")
 
-doneBtn = Button(root, image = done_btn, command=Click, borderwidth = 0)
+doneBtn = tk.Button(root, image = done_btn, command=Click, borderwidth = 0)
 doneBtn.grid(row = 5, column = 0, pady = 10, sticky="nsew")
 doneBtn.grid_rowconfigure(0, weight = 1)
 doneBtn.grid_columnconfigure(0, weight = 1)
