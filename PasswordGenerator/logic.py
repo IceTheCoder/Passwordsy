8e712678e2e15f0df0bdc806fa457ef9f7dae301
IceTheDev2 <115871297+IceTheDev2@users.noreply.github.com>
2022-12-05 14:42:25 +0200

    Renamed variables to be clearer.
    * Renamed root to window
    * Renamed welcome to title
    * Changed '(up to 100) Number of characters' to 'Number of characters (up to 100):'
    * Renamed input to input_box
    * Corrected typo in StartPasswordGeneration DOCSTRING (diplay became displays)
    * Renamed length to requested_length

--- PasswordGenerator/logic.py
+++ PasswordGenerator/logic.py
@@ -3,17 +3,18 @@ import string
 
 characters = string.ascii_letters + string.punctuation
 
-def GeneratePassword(length) -> str:
+def GeneratePassword(requested_length) -> str:
     '''
-    Checks if the requested length is valid,
+    Checks if the requested requested_length is valid,
     returns a password if it is,
     raises a value error if it's not.
     '''
-    if 1 <= length:
-        if length > 100:
-            length = 100
 
-        return ''.join(secrets.choice(characters) for i in range(length)) # This is where the password itself is generated.
+    if 1 <= requested_length:
+        if requested_length > 100:
+            requested_length = 100
+
+        return ''.join(secrets.choice(characters) for i in range(requested_length)) # This is where the password itself is generated.
 
     else:
         raise ValueError
