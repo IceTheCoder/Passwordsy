da3089faabd35be8bd19ff0e198d0fabb15a9c7c
IceTheDev2 <115871297+IceTheDev2@users.noreply.github.com>
2023-01-21 16:07:45 +0200

    Added a second tab for future features

--- PasswordGenerator/main.py
+++ PasswordGenerator/main.py
@@ -1,15 +1,37 @@
 import tkinter as tk
 import logic
-
+from tkinter import ttk as ttk
 
 def main():
     window = tk.Tk()
+    notebook = ttk.Notebook(window)
+    notebook.grid(column = 0, row = 0)
 
     window_width = 854
     window_height = 350
     resizable_ability = False
     app_name = 'Passwordsy'
+    title_font = 'Helvetica 24'
     description_font = 'Helvetica 12'
+
+    generate_password_frame = ttk.Frame(notebook, width = 854, height = 350)
+    password_strength_frame = ttk.Frame(notebook, width = 854, height = 350)
+
+    generate_password_frame.grid(column = 0, row = 1)
+    password_strength_frame.grid(column = 0, row = 0)
+
+    notebook.add(generate_password_frame, text = 'Generate a password')
+    notebook.add(password_strength_frame, text = 'Check a password\'s strength')
+
+    title = tk.Label(generate_password_frame, text=app_name, font = title_font)
+    title.place(relx = 0.5, rely = 0.0, anchor = 'n')
+
+    tip = tk.Label(generate_password_frame, text='CTRL + C to copy', font=description_font)
+    tip.place(relx = 0.5, rely = 0.1, anchor = 'n')
+
+    question = tk.Label(generate_password_frame, text='Number of characters (up to 100):', font=description_font)
+    question.place(relx = 0.5, rely = 0.15, anchor = 'n')
+
 
     window.minsize(window_width, window_height)
     window.maxsize(window_width, window_height)
@@ -17,15 +39,6 @@ def main():
     window.resizable(resizable_ability, resizable_ability)
     window.iconphoto(False, tk.PhotoImage(file='logo.png'))
     window.title(app_name)
-
-    title = tk.Label(window, text=app_name, font='Helvetica 24')
-    title.grid(row=1)
-
-    question = tk.Label(window, text='Number of characters (up to 100):', font=description_font)
-    question.grid(row=3)
-
-    tip = tk.Label(window, text='CTRL + C to copy \nCTRL + V to paste', font=description_font)
-    tip.grid(row=2)
 
     def create_password_labels(_) -> None:
         """
@@ -43,13 +56,13 @@ def main():
 
         error = 'An error occured. Try again with a whole number greater than 0.'
 
-        password_label_1 = tk.Text(window, width=password_width, height=password_height,
+        password_label_1 = tk.Text(generate_password_frame, width=password_width, height=password_height,
                                    borderwidth=password_border_width, font=password_font)
-        password_label_2 = tk.Text(window, width=password_width, height=password_height,
+        password_label_2 = tk.Text(generate_password_frame, width=password_width, height=password_height,
                                    borderwidth=password_border_width, font=password_font)
-        password_label_3 = tk.Text(window, width=password_width, height=password_height,
+        password_label_3 = tk.Text(generate_password_frame, width=password_width, height=password_height,
                                    borderwidth=password_border_width, font=password_font)
-        password_label_4 = tk.Text(window, width=password_width, height=password_height,
+        password_label_4 = tk.Text(generate_password_frame, width=password_width, height=password_height,
                                    borderwidth=password_border_width, font=password_font)
         password_labels = [password_label_1, password_label_2, password_label_3, password_label_4]
 
@@ -62,9 +75,9 @@ def main():
             input_box.delete(0, 'end')
             show_password(password_label_1, error, 0)
 
-    input_box = tk.Entry(window, width=10, borderwidth=2)
+    input_box = tk.Entry(generate_password_frame, width=10, borderwidth=2)
     input_box.bind("<Return>", create_password_labels)
-    input_box.grid(row=4)
+    input_box.place(relx = 0.5, rely = 0.2225, anchor = 'n')
 
     def show_password(label, text, index) -> None:
         """
@@ -76,19 +89,18 @@ def main():
         :param int index: Which label is being displayed.
         """
         label.insert(1.0, text)
-        label.grid(row=6 + index, pady=5)
+        label.place(relx = 0.5, rely = 0.485 + (index / 10), anchor = 'n')
         label.configure(state='disabled')  # Makes the text uneditable.
 
     done_button_image = tk.PhotoImage(file='doneButton.png')
 
     done_button_border_width = 0
 
-    done_btn = tk.Button(window, image=done_button_image, borderwidth=done_button_border_width,
-                         command=lambda: create_password_labels(None))
-    done_btn.grid(row=5, column=0, pady=10)
+    done_btn = tk.Button(generate_password_frame, image=done_button_image, borderwidth=done_button_border_width,
+                         command = lambda: create_password_labels(None))
+    done_btn.place(relx = 0.5, rely = 0.31, anchor = 'n')
 
     window.mainloop()
-
 
 if __name__ == "__main__":
     exit(main())
