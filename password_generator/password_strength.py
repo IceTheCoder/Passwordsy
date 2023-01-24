bdeaeedb90cbda12adba197e48522d52d3f17a0b
IceTheDev2 <115871297+IceTheDev2@users.noreply.github.com>
2023-01-24 20:57:36 +0200

    Added some GUI to the password strength frame
    Specifically, a title and an input box

--- password_generator/password_strength.py
+++ password_generator/password_strength.py
@@ -1,5 +1,13 @@
 import tkinter as tk
 
+title_font = 'Helvetica 24'
+
 def show_password_strength_frame(frame):
-    my_label = tk.Label(frame, text = 'Hello, World!')
-    my_label.place(relx = 0.1, rely = 0, anchor = 'n')
+    my_label = tk.Label(frame, text = 'Type your password to check its strength', font = title_font)
+    my_label.place(relx = 0.5, rely = 0, anchor = 'n')
+
+    input_box = tk.Entry(frame, width = 16, borderwidth = 2)
+    input_box.place(relx = 0.5, rely = 0.125, anchor = 'n')
+
+    while True:
+        pass
