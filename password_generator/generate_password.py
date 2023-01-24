3aedec59311b34f9cddb1c6ab2c59de18abe9c46
IceTheDev2 <115871297+IceTheDev2@users.noreply.github.com>
2023-01-24 17:54:06 +0200

    Cleaned code

---
+++ password_generator/generate_password.py
@@ -0,0 +1,129 @@
+import secrets as secrets
+import string as string
+import tkinter as tk
+
+title_font = 'Helvetica 24'
+description_font = 'Helvetica 12'
+
+characters = string.ascii_letters + string.punctuation + string.digits
+
+def show_generate_password_frame(frame, done_btn_image) -> None:
+    '''
+    Called upon starting the program,
+    this function creates the password generation frame and its contents,
+    and serves as a hub for all other password-generation functions.
+
+    Parameters
+    ----------
+    frame: ttk.frame
+        The "generate password" frame upon which the objects of this function will be placed.
+    done_btn_image: ImageTk.PhotoImage
+        The image used for the done button.
+    '''
+    frame_title_text = 'Generate password'
+
+    frame_title = tk.Label(frame, text = frame_title_text, font = title_font)
+    frame_title.place(relx = 0.5, rely = 0.0, anchor = 'n')
+
+    tip = tk.Label(frame, text = 'CTRL + C to copy', font = description_font)
+    tip.place(relx = 0.5, rely = 0.1, anchor = 'n')
+
+    question = tk.Label(frame, text = 'Number of characters (up to 100):', font = description_font)
+    question.place(relx = 0.5, rely = 0.16, anchor = 'n')
+
+    def create_password_labels(event) -> None:
+        '''
+        Called upon clicking the done button or pressing the ENTER key,
+        this function creates the password(s) or error label(s),
+        and calls the show_password() function to show the passwords or the error.
+
+        Parameters
+        ----------
+        event:
+            Necessary for initiating the function when pressing the ENTER key
+
+        Raises
+        ------
+        ValueError
+            If an invalid value is placed in the input box.
+        '''
+
+        password_width = 100
+        password_height = 1
+        password_border_width = 0
+        password_font = 'Consolas 11'
+
+        error = 'An error occured. Try again with a whole number greater than 0.'
+
+        password_label_1 = tk.Text(frame, width = password_width, height = password_height,
+                                   borderwidth = password_border_width, font = password_font)
+        password_label_2 = tk.Text(frame, width = password_width, height = password_height,
+                                   borderwidth = password_border_width, font = password_font)
+        password_label_3 = tk.Text(frame, width = password_width, height = password_height,
+                                   borderwidth = password_border_width, font = password_font)
+        password_label_4 = tk.Text(frame, width = password_width, height = password_height,
+                                   borderwidth = password_border_width, font = password_font)
+
+        password_labels = [password_label_1, password_label_2, password_label_3, password_label_4]
+
+        try:
+            for password_label in password_labels:
+                requested_length = int(input_box.get())
+                password = generate_password(requested_length)
+                show_password(password_label, password, password_labels.index(password_label))
+        except ValueError:
+            input_box.delete(0, 'end')
+            show_password(password_label_1, error, 0)
+
+    global input_box
+    input_box = tk.Entry(frame, width = 10, borderwidth = 2)
+    input_box.bind('<Return>', create_password_labels)
+    input_box.place(relx = 0.5, rely = 0.2325, anchor = 'n')
+
+    done_btn = tk.Button(frame, image = done_btn_image, borderwidth = 0, command = lambda: create_password_labels(None))
+    done_btn.place(relx = 0.5, rely = 0.31, anchor = 'n')
+
+    def show_password(label, text, index) -> None:
+        '''
+        Called by the create_password_labels function,
+        after password generation or attempted password generation,
+        this function displays the passwords or an error.
+
+        Parameters
+        ----------
+        label: tkinter.Text
+            Each individual password label one by one if passwords are generated,
+            or the first password label if an error is generated.
+        text: str
+            The password or the error.
+        index:
+            The index of the password label being shown is necessary for placing it correctly on the screen.
+        '''
+
+        label.insert(1.0, text)
+        label.place(relx = 0.5, rely = 0.485 + (index / 10), anchor = 'n')
+        label.configure(state = 'disabled')  # Makes the text uneditable.
+
+    def generate_password(requested_length) -> str:
+        '''
+        Called by the create_password_labels function,
+        (upon clicking the done button or the ENTER key),
+        this function checks if the requested_length is valid,
+        returns a password if it is,
+        raises a value error if it's not.
+
+        Parameters
+        ----------
+        requested_length: int
+            The length requested by the user.
+        
+        Raises
+        ------
+        ValueError
+            If the requested length is not an integer greater than 0.
+        '''
+
+        if requested_length > 0:
+            return ''.join(secrets.choice(characters) for _ in range(min(int(requested_length), 100))) # This is where the password itself is generated
+        else:
+            raise ValueError
--- password_generator/main.py
+++ password_generator/main.py
@@ -1,104 +1,56 @@
 import tkinter as tk
-import logic
 from tkinter import ttk as ttk
+import generate_password
+from PIL import ImageTk, Image
 
-def main():
+def main() -> None:
+    '''
+    Called upon starting the program,
+    it generates the window, notebook and frames for the app.
+    '''
     window = tk.Tk()
     notebook = ttk.Notebook(window)
     notebook.grid(column = 0, row = 0)
 
     window_width = 854
     window_height = 350
-    resizable_ability = False
+    horizontal_resizable_ability = False
+    vertical_resizable_ability = False
     app_name = 'Passwordsy'
-    frame_name = 'Generate password'
-    title_font = 'Helvetica 24'
-    description_font = 'Helvetica 12'
-
-    generate_password_frame = ttk.Frame(notebook, width = 854, height = 350)
-    password_strength_frame = ttk.Frame(notebook, width = 854, height = 350)
-
-    generate_password_frame.grid(column = 0, row = 1)
-    password_strength_frame.grid(column = 0, row = 0)
-
-    notebook.add(generate_password_frame, text = frame_name)
-    notebook.add(password_strength_frame, text = 'Check a password\'s strength')
-
-    title = tk.Label(generate_password_frame, text = app_name, font = title_font)
-    title.place(relx = 0.5, rely = 0.0, anchor = 'n')
-
-    tip = tk.Label(generate_password_frame, text='CTRL + C to copy', font = description_font)
-    tip.place(relx = 0.5, rely = 0.1, anchor = 'n')
-
-    question = tk.Label(generate_password_frame, text = 'Number of characters (up to 100):', font = description_font)
-    question.place(relx = 0.5, rely = 0.16, anchor = 'n')
 
     window.minsize(window_width, window_height)
     window.maxsize(window_width, window_height)
-    window.grid_columnconfigure(0, weight=1)
-    window.resizable(resizable_ability, resizable_ability)
-    window.iconphoto(False, tk.PhotoImage(file='logo.png'))
+
+    window.grid_columnconfigure(0, weight = 1)
+    window.resizable(horizontal_resizable_ability, vertical_resizable_ability)
+    window.iconphoto(False, tk.PhotoImage(file = 'logo.png'))
     window.title(app_name)
 
-    def create_password_labels(_) -> None:
+    class GeneratePasswordFrame:
         '''
-        Called upon clicking the done button or pressing the ENTER key,
-        this function creates the password or error label,
-        and calls the show_password(label, text, index) function to show the passwords or the error.
+        A class that contains the creation of the "generate password" frame.
 
-        :param _: Necessary for calling the function when pressing the ENTER key.
+        ...
+
+        Attributes
+        ----------
+        generate_password_frame: ttk.frame
+            The generate password frame
+        done_btn_image: ImageTk.PhotoImage
+            The photo that will be used for the done button.
         '''
+        generate_password_frame = ttk.Frame(notebook, width = 854, height = 350)
+        generate_password_frame.grid(column = 0, row = 1)
+        notebook.add(generate_password_frame, text = 'Generate password')
+        done_btn_image = ImageTk.PhotoImage(Image.open('done_button.png'))
 
-        password_width = 100
-        password_height = 1
-        password_border_width = 0
-        password_font = 'Consolas 11'
+        generate_password.show_generate_password_frame(generate_password_frame, done_btn_image)
 
-        error = 'An error occured. Try again with a whole number greater than 0.'
-
-        password_label_1 = tk.Text(generate_password_frame, width = password_width, height = password_height,
-                                   borderwidth = password_border_width, font = password_font)
-        password_label_2 = tk.Text(generate_password_frame, width = password_width, height = password_height,
-                                   borderwidth = password_border_width, font = password_font)
-        password_label_3 = tk.Text(generate_password_frame, width = password_width, height = password_height,
-                                   borderwidth = password_border_width, font = password_font)
-        password_label_4 = tk.Text(generate_password_frame, width = password_width, height = password_height,
-                                   borderwidth = password_border_width, font = password_font)
-        password_labels = [password_label_1, password_label_2, password_label_3, password_label_4]
-
-        try:
-            for password_label in password_labels:
-                requested_length = int(input_box.get())
-                password = logic.generate_password(requested_length)
-                show_password(password_label, password, password_labels.index(password_label))
-
-        except ValueError:
-            input_box.delete(0, 'end')
-            show_password(password_label_1, error, 0)
-
-    input_box = tk.Entry(generate_password_frame, width = 10, borderwidth = 2)
-    input_box.bind("<Return>", create_password_labels)
-    input_box.place(relx = 0.5, rely = 0.2325, anchor = 'n')
-
-    def show_password(label, text, index) -> None:
-        """
-        Called by the CreatePasswordLabels function,
-        this function displays the passwords or an error.
-
-        :param tk.Text label: Each individual password or error label.
-        :param str text: The generated password or the error.
-        :param int index: Which label is being displayed.
-        """
-        label.insert(1.0, text)
-        label.place(relx = 0.5, rely = 0.485 + (index / 10), anchor = 'n')
-        label.configure(state = 'disabled')  # Makes the text uneditable.
-
-    done_button_image = tk.PhotoImage(file='done_button.png')
-
-    done_btn = tk.Button(generate_password_frame, image = done_button_image, borderwidth = 0, command = lambda: create_password_labels(None))
-    done_btn.place(relx = 0.5, rely = 0.31, anchor = 'n')
+    password_strength_frame = ttk.Frame(notebook, width = 854, height = 350)
+    password_strength_frame.grid(column = 0, row = 0)
+    notebook.add(password_strength_frame, text = 'Check a password\'s strength')
 
     window.mainloop()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     exit(main())
