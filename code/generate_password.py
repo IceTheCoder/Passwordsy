418fcfdd9f449c9339a637684011f0cb00969b38
IceTheDev2 <115871297+IceTheDev2@users.noreply.github.com>
2023-02-17 17:11:13 +0200

    Implemented the invalid_input_error

--- code/generate_password.py
+++ code/generate_password.py
@@ -14,9 +14,9 @@ password_height = 1
 password_border_width = 0
 password_font = 'Consolas 11'
 
-invalid_input_error = 'An error occurred. Try again with a whole number between 6 and 100.'
+invalid_input_error = 'An error occurred. Try again with a whole number between 4 and 100.'
 no_character_set_error = 'An error occurred. Try again with at least 1 character set.'
-double_error = 'An error occurred. Try again with at least 1 character set and a whole number between 6 and 100.'
+double_error = 'An error occurred. Try again with at least 1 character set and a whole number between 4 and 100.'
 
 def show_copy_button(event) -> None:
     '''
@@ -174,8 +174,6 @@ def show_generate_password_frame(frame, done_btn_image) -> None:
             The length requested by the user.
         '''
 
-        print(lowercase_letters_var.get(), uppercase_letters_var.get(), digits_var.get(), punctuation_var.get())
-
         if lowercase_letters_var.get() == 0 and uppercase_letters_var.get() == 0 and digits_var.get() == 0 and punctuation_var.get() == 0:
             try:
                 if 4 <= int(requested_length) <= 100:
@@ -186,6 +184,12 @@ def show_generate_password_frame(frame, done_btn_image) -> None:
             except:
                 input_box.delete(0, 'end')
                 return double_error
+
+        try:
+            if not 4 <= int(requested_length) <= 100:
+                return invalid_input_error
+        except:
+            return invalid_input_error
 
         # Define all character sets that will be used in the password
         character_sets = []
