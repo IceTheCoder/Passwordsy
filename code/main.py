2595dac112e1ab43462ca10f94bcb1aeb9ac4c21
IceTheDev2 <115871297+IceTheDev2@users.noreply.github.com>
2023-02-23 15:43:57 +0200

    Cleaned code to be in accordance with PEP8

--- code/main.py
+++ code/main.py
@@ -5,6 +5,7 @@ from PIL import ImageTk, Image
 import generate_password_gui
 import password_strength_gui
 
+
 def main():
     '''
     Called upon starting the program,
@@ -14,16 +15,16 @@ def main():
     '''
     window = tk.Tk()
 
-    notebook = ttk.Notebook(window, width = 1000, height = 325)
-    notebook.grid(column = 0, row = 0)
+    notebook = ttk.Notebook(window, width=1000, height=325)
+    notebook.grid(column=0, row=0)
 
     # Center the notebook
-    window.grid_columnconfigure(0, weight = 1)
-    window.grid_rowconfigure(0, weight = 1)
+    window.grid_columnconfigure(0, weight=1)
+    window.grid_rowconfigure(0, weight=1)
 
     app_name = 'Passwordsy'
 
-    window.iconphoto(False, tk.PhotoImage(file = 'logo.png'))
+    window.iconphoto(False, tk.PhotoImage(file='logo.png'))
     window.title(app_name)
 
     done_btn_image = ImageTk.PhotoImage(Image.open('done_btn.png'))
@@ -43,15 +44,15 @@ def main():
             The 'generate password' frame
         '''
         generate_password_frame = tk.Frame(window)
-        generate_password_frame.grid(column = 0, row = 0)
+        generate_password_frame.grid(column=0, row=0)
 
         # Expand some widgets' rows and columns to take up the entire window
-        generate_password_frame.grid_columnconfigure(0, weight = 1)
-        generate_password_frame.grid_rowconfigure(0, weight = 1)
-        generate_password_frame.grid_rowconfigure(1, weight = 1)
-        generate_password_frame.grid_rowconfigure(2, weight = 1)
-        generate_password_frame.grid_rowconfigure(3, weight = 1)
-        generate_password_frame.grid_rowconfigure(4, weight = 1)
+        generate_password_frame.grid_columnconfigure(0, weight=1)
+        generate_password_frame.grid_rowconfigure(0, weight=1)
+        generate_password_frame.grid_rowconfigure(1, weight=1)
+        generate_password_frame.grid_rowconfigure(2, weight=1)
+        generate_password_frame.grid_rowconfigure(3, weight=1)
+        generate_password_frame.grid_rowconfigure(4, weight=1)
 
         notebook.add(generate_password_frame, text = 'Generate password')
 
@@ -69,17 +70,17 @@ def main():
             The password strength frame
         '''
         password_strength_frame = tk.Frame(notebook)
-        password_strength_frame.grid(column = 0, row = 0)
+        password_strength_frame.grid(column=0, row=0)
 
         # Expand widgets to take up the entire window
-        password_strength_frame.grid_columnconfigure(0, weight = 1)
-        password_strength_frame.grid_rowconfigure(0, weight = 1)
-        password_strength_frame.grid_rowconfigure(1, weight = 1)
-        password_strength_frame.grid_rowconfigure(2, weight = 1)
-        password_strength_frame.grid_rowconfigure(3, weight = 1)
-        password_strength_frame.grid_rowconfigure(4, weight = 1)
-        password_strength_frame.grid_rowconfigure(5, weight = 1)
-        password_strength_frame.grid_rowconfigure(6, weight = 1)
+        password_strength_frame.grid_columnconfigure(0, weight=1)
+        password_strength_frame.grid_rowconfigure(0, weight=1)
+        password_strength_frame.grid_rowconfigure(1, weight=1)
+        password_strength_frame.grid_rowconfigure(2, weight=1)
+        password_strength_frame.grid_rowconfigure(3, weight=1)
+        password_strength_frame.grid_rowconfigure(4, weight=1)
+        password_strength_frame.grid_rowconfigure(5, weight=1)
+        password_strength_frame.grid_rowconfigure(6, weight=1)
 
 
         notebook.add(password_strength_frame, text = 'Password strength')
@@ -87,6 +88,7 @@ def main():
         password_strength_gui.create_password_strength_frame(password_strength_frame)
 
     window.mainloop()
+
 
 if __name__ == '__main__':
     exit(main())
