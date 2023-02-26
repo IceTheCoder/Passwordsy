7276b77fbe1db0d86e281a24826bc6ea978a0ecf
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-02-26 15:50:05 +0200

    Search the wordlist for the word with that number

--- code/password_generation/diceware_gui.py
+++ code/password_generation/diceware_gui.py
@@ -1,31 +1,8 @@
 import tkinter as tk
-import secrets
 
-numbers = [1, 2, 3, 4, 5, 6]
+import password_generation.diceware_logic as logic
 
-# The wordlist is from https://github.com/ulif/diceware
-diceware_wordlist = open('password_generation\wordlist_en_eff.txt', 'r')
-diceware_wordlist_read = diceware_wordlist.readlines()
-wordlist = {}
-
-# Make a 'wordlist' dictionary, where the key is the dice roll numbers, and the value is the specific word.
-for line in diceware_wordlist_read:
-    # Split the line into a list of dice roll numbers and the corresponding word
-    line_parts = line.strip().split('\t')
-    dice_rolls = line_parts[0]
-    word = line_parts[1]
-
-    # Add the dice roll numbers and word to the dictionary
-    wordlist[dice_rolls] = word
 
 def create_diceware_frame(frame):
-    def roll_dice():
-        dice_roll = ''
-        i = 0
-        while i < 5:
-            dice_roll += str(secrets.choice(numbers))
-            i += 1
-
-
-    roll_dice_button = tk.Button(frame, text='Roll dice', command=roll_dice)
+    roll_dice_button = tk.Button(frame, text='Roll dice', command=logic.roll_dice)
     roll_dice_button.grid(row=0,column=0)
---
+++ code/password_generation/diceware_logic.py
@@ -0,0 +1,32 @@
+import secrets
+
+numbers = [1, 2, 3, 4, 5, 6]
+
+# The wordlist is from https://github.com/ulif/diceware
+diceware_wordlist = open('password_generation\wordlist_en_eff.txt', 'r')
+diceware_wordlist_read = diceware_wordlist.readlines()
+wordlist = {}
+
+# Make a 'wordlist' dictionary, where the key is the dice roll numbers, and the value is the specific word.
+for line in diceware_wordlist_read:
+    # Split the line into a list of dice roll numbers and the corresponding word
+    line_parts = line.strip().split('\t')
+    dice_rolls = line_parts[0]
+    word = line_parts[1]
+
+    # Add the dice roll numbers and word to the dictionary
+    wordlist[dice_rolls] = word
+
+
+def get_word_from_dice_roll(number):
+    print(wordlist[number])
+    print(number)
+
+
+def roll_dice() -> None:
+    dice_roll = ''
+    i = 0
+    while i < 5:
+        dice_roll += str(secrets.choice(numbers))
+        i += 1
+    get_word_from_dice_roll(dice_roll)
