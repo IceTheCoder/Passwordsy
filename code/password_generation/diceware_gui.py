7701b63bb8eb3e1ee23da6c96ea4054d32d531d6
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-02-26 16:00:46 +0200

    Update diceware_gui.py

--- code/password_generation/diceware_gui.py
+++ code/password_generation/diceware_gui.py
@@ -7,11 +7,11 @@ def create_diceware_frame(frame):
     roll_dice_button = tk.Button(frame, text='Roll dice', command=lambda: display_words(logic.roll_dice()))
     roll_dice_button.grid(row=0, column=0)
 
-    word_widget = tk.Text(frame)
-    number_widget = tk.Text(frame)
-    word_widget.grid(row=1, column=0)
+    pair_widget = tk.Text(frame)
+    pair_widget.grid(row=1, column=0)
+
     def display_words(pair):
-        word_widget.configure(state='normal')
-        word_widget.delete('1.0', 'end')
-        word_widget.insert('1.0', pair)
-        word_widget.configure(state='disabled')
+        pair_widget.configure(state='normal')
+        pair_widget.delete('1.0', 'end')
+        pair_widget.insert('1.0', pair)
+        pair_widget.configure(state='disabled')
