87f41b892caaefb4b16086d40b3d268dc6493dda
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-02-26 13:37:39 +0200

    Update diceware_gui.py

--- code/password_generation/diceware_gui.py
+++ code/password_generation/diceware_gui.py
@@ -1,6 +1,18 @@
 import tkinter as tk
+import secrets
+
+numbers = [1, 2, 3, 4, 5, 6]
 
 
 def create_diceware_frame(frame):
-    roll_dice_button = tk.Button(frame, text='Roll dice')
+    def roll_dice():
+        dice_roll = ''
+        i = 0
+        while i < 5:
+            dice_roll += str(secrets.choice(numbers))
+            i += 1
+        print(dice_roll)
+
+
+    roll_dice_button = tk.Button(frame, text='Roll dice', command=roll_dice)
     roll_dice_button.grid(row=0,column=0)
