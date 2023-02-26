dd388b24fb4a280ba313fd05aa51e38a303bac37
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-02-26 11:16:02 +0200

    Added a diceware frame

--- code/other_methods_gui.py
+++ code/other_methods_gui.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from PIL import ImageTk, Image
 
+import diceware_gui as diceware
+
 
 def create_other_methods_window():
     other_methods_window = tk.Toplevel()
@@ -9,3 +11,17 @@ def create_other_methods_window():
 
     other_methods_window.iconphoto(False, tk.PhotoImage(file='logo.png'))
     other_methods_window.title(window_title)
+
+    diceware_frame = tk.LabelFrame(other_methods_window, text='Diceware')
+    diceware_frame.grid(row=0, column=0)
+
+    sentence_input_frame = tk.LabelFrame(other_methods_window, text='Input a sentence')
+    sentence_input_frame.grid(row=0, column=1)
+
+    diceware.create_diceware_frame(diceware_frame)
+    
+
+    # Expand some widgets' rows and columns to take up the entire window
+    other_methods_window.rowconfigure(0, weight=1)
+    other_methods_window.columnconfigure(0, weight=1)
+    other_methods_window.columnconfigure(1, weight=1)
