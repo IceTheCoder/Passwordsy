0aab5553c79d6811cded47bd9b8dd25509b830a5
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-02-26 17:26:59 +0200

    Added DOCSTRINGS

--- code/password_generation/sentence_input_logic.py
+++ code/password_generation/sentence_input_logic.py
@@ -1,2 +1,18 @@
+
 def produce_password(sentence):
-    print('Hello, world!')
+    """
+    Called as the user types a sentence to be turned into a password,
+    this function takes the sentence the user is typing,
+    splits it into words,
+    and returns the first letter of each word,
+    as well as any punctuation and numbers.
+
+    Parameters
+    ----------
+    sentence: str
+        The sentence the user is typing.
+    """
+
+    split_sentence = sentence.split(' ')
+
+
