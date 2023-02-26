50fa18d516fc68488bb66b3aa78e488e8a8fb12d
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-02-26 18:04:57 +0200

    Update generate_password_gui.py

--- code/password_generation/generate_password_gui.py
+++ code/password_generation/generate_password_gui.py
@@ -48,7 +48,19 @@ def create_generate_password_frame(frame, done_btn_image) -> None:
                                borderwidth=password_border_width, font=password_font)
     password_labels = [password_label_1, password_label_2, password_label_3, password_label_4]
 
-    def show_password(index, button):
+    def show_password(index, button) -> None:
+        """
+        Called when the user clicks one of the 4 show buttons,
+        this function clears the specific password_label,
+        and inserts the generated password inside of it
+
+        Parameters
+        ----------
+        index: int
+            The number of the button that was clicked.
+        button: tk.Button
+            The button that was clicked.
+        """
         global passwords
         if passwords:
             password_labels[index].configure(state='normal')
@@ -65,6 +77,17 @@ def create_generate_password_frame(frame, done_btn_image) -> None:
         button.configure(text='Hide', command=lambda: hide_password(index, button))
 
     def hide_password(index, button):
+        """
+
+        Parameters
+        ----------
+        index
+        button
+
+        Returns
+        -------
+
+        """
         global passwords
         if passwords:
             password_labels[index].configure(state='normal')
@@ -79,6 +102,9 @@ def create_generate_password_frame(frame, done_btn_image) -> None:
         button.configure(text='Show', command=lambda: show_password(index, button))
 
     def show_all_passwords():
+        """
+
+        """
         global show_hide_all_button
         for index, label in enumerate(password_labels):
             label.configure(state='normal')
@@ -88,6 +114,9 @@ def create_generate_password_frame(frame, done_btn_image) -> None:
         show_hide_all_button.configure(text='Hide all', command=hide_all_passwords)
 
     def hide_all_passwords():
+        """
+
+        """
         global show_hide_all_button
         for index, label in enumerate(password_labels):
             label.configure(state='normal')
