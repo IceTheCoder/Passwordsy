a40cb2f179422eeb15a143ab300b60cab624805f
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-02-26 14:06:24 +0200

    Update diceware_gui.py

--- code/password_generation/diceware_gui.py
+++ code/password_generation/diceware_gui.py
@@ -3,6 +3,20 @@ import secrets
 
 numbers = [1, 2, 3, 4, 5, 6]
 
+# The wordlist is from https://github.com/ulif/diceware
+diceware_wordlist = open('password_generation\wordlist_en_eff.txt', 'r')
+diceware_wordlist_read = diceware_wordlist.readlines()
+wordlist = {}
+
+# Make a 'wordlist' dictionary, where the key is the dice roll numbers, and the value is the specific word.
+for line in diceware_wordlist_read:
+    # Split the line into a list of dice roll numbers and the corresponding word
+    line_parts = line.strip().split('\t')
+    dice_rolls = line_parts[0]
+    word = line_parts[1]
+
+    # Add the dice roll numbers and word to the dictionary
+    wordlist[dice_rolls] = word
 
 def create_diceware_frame(frame):
     def roll_dice():
@@ -11,7 +25,6 @@ def create_diceware_frame(frame):
         while i < 5:
             dice_roll += str(secrets.choice(numbers))
             i += 1
-        print(dice_roll)
 
 
     roll_dice_button = tk.Button(frame, text='Roll dice', command=roll_dice)
