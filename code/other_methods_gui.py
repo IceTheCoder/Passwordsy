0ced66eed66e68cb8e81d0833b7cc7a1da61fae0
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-02-26 11:08:50 +0200

    Update other_methods_gui.py

--- code/other_methods_gui.py
+++ code/other_methods_gui.py
@@ -1,5 +1,11 @@
 import tkinter as tk
+from PIL import ImageTk, Image
 
 
 def create_other_methods_window():
-    other_methods_window = tk.Tk()
+    other_methods_window = tk.Toplevel()
+
+    window_title = 'Try other methods...'
+
+    other_methods_window.iconphoto(False, tk.PhotoImage(file='logo.png'))
+    other_methods_window.title(window_title)
