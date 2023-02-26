2aa971b90054a4cdaa8bd5c5dcd7f8110fe71c2d
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-02-26 11:51:55 +0200

    Update other_methods_gui.py

--- code/password_generation/other_methods_gui.py
+++ code/password_generation/other_methods_gui.py
@@ -9,7 +9,7 @@ def create_other_methods_window():
 
     window_title = 'Try other methods...'
 
-    other_methods_window.iconphoto(False, tk.PhotoImage(file='../logo.png'))
+    other_methods_window.iconphoto(False, tk.PhotoImage(file='logo.png'))
     other_methods_window.title(window_title)
 
     diceware_frame = tk.LabelFrame(other_methods_window, text='Diceware')
