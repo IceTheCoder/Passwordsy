ab29996dece16044060e1dc31cfb2a2430b3ee57
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-02-27 16:02:39 +0200

    Managed to make the only one pop-up window exist

--- code/password_generation/other_methods_gui.py
+++ code/password_generation/other_methods_gui.py
@@ -6,32 +6,59 @@ import tkinter as tk
 from password_generation import diceware_gui as diceware
 from password_generation import sentence_input_gui as sentence_input
 
+global other_methods_window
 
-def create_other_methods_window(other_methods_window_open) -> None:
+
+def create_other_methods_window() -> None:
     """
     Called upon app startup, this function prepares the other methods window for the user,
     by creating 2 frames: 1 for diceware, and 1 for sentence input.
     """
-    other_methods_window_open = True
-    other_methods_window = tk.Toplevel()
 
-    window_title = 'Try other methods...'
+    global other_methods_window
+    try:
+        if other_methods_window.winfo_exists() == 0:
+            print('Hello, world!')
+            other_methods_window = tk.Toplevel()
+            window_title = 'Try other methods...'
 
-    other_methods_window.iconphoto(False, tk.PhotoImage(file='textures/logo.png'))
-    other_methods_window.title(window_title)
+            other_methods_window.iconphoto(False, tk.PhotoImage(file='textures/logo.png'))
+            other_methods_window.title(window_title)
 
-    diceware_frame = tk.LabelFrame(other_methods_window, text='Diceware')
-    diceware_frame.grid(row=0, column=0)
+            diceware_frame = tk.LabelFrame(other_methods_window, text='Diceware')
+            diceware_frame.grid(row=0, column=0)
 
-    sentence_input_frame = tk.LabelFrame(other_methods_window, text='Input a sentence')
-    sentence_input_frame.grid(row=0, column=1)
+            sentence_input_frame = tk.LabelFrame(other_methods_window, text='Input a sentence')
+            sentence_input_frame.grid(row=0, column=1)
 
-    diceware.create_diceware_frame(diceware_frame)
-    sentence_input.create_sentence_input_frame(sentence_input_frame)
+            diceware.create_diceware_frame(diceware_frame)
+            sentence_input.create_sentence_input_frame(sentence_input_frame)
 
-    # Expand some widgets' rows and columns to take up the entire window
-    other_methods_window.rowconfigure(0, weight=1)
-    other_methods_window.columnconfigure(0, weight=1)
-    other_methods_window.columnconfigure(1, weight=1)
+            # Expand some widgets' rows and columns to take up the entire window
+            other_methods_window.rowconfigure(0, weight=1)
+            other_methods_window.columnconfigure(0, weight=1)
+            other_methods_window.columnconfigure(1, weight=1)
+            other_methods_window.mainloop()
+        else:
+            return
+    except Error:
+        other_methods_window = tk.Toplevel()
+        window_title = 'Try other methods...'
 
-    other_methods_window.mainloop()
+        other_methods_window.iconphoto(False, tk.PhotoImage(file='textures/logo.png'))
+        other_methods_window.title(window_title)
+
+        diceware_frame = tk.LabelFrame(other_methods_window, text='Diceware')
+        diceware_frame.grid(row=0, column=0)
+
+        sentence_input_frame = tk.LabelFrame(other_methods_window, text='Input a sentence')
+        sentence_input_frame.grid(row=0, column=1)
+
+        diceware.create_diceware_frame(diceware_frame)
+        sentence_input.create_sentence_input_frame(sentence_input_frame)
+
+        # Expand some widgets' rows and columns to take up the entire window
+        other_methods_window.rowconfigure(0, weight=1)
+        other_methods_window.columnconfigure(0, weight=1)
+        other_methods_window.columnconfigure(1, weight=1)
+        other_methods_window.mainloop()
