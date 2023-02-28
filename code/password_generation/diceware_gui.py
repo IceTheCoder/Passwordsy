4a9cd0a54dcfa380367b4c9e691f2e9d524fabbd
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-02-28 15:06:23 +0200

    Update diceware_gui.py

--- code/password_generation/diceware_gui.py
+++ code/password_generation/diceware_gui.py
@@ -69,21 +69,24 @@ def create_diceware_frame(frame):
             Contains the pairs of dice roll numbers and related words according to the dice ware wordlist.
         """
         global number_of_dicerolls
-        number_of_dicerolls += 1
-        (diceroll, word), = pair.items()
+        if number_of_dicerolls < 35:
+            number_of_dicerolls += 1
+            (diceroll, word), = pair.items()
 
-        diceroll_widget = tk.Text(frame, font=word_font, height=1, width=len(word))
-        diceroll_widget.grid(row=2 + 2 * ((number_of_dicerolls - 1) // 5), column=(-1 + number_of_dicerolls) % 5)
-        diceroll_widget.configure(state='normal')
-        diceroll_widget.delete('1.0', 'end')
-        diceroll_widget.insert('1.0', str(diceroll))
-        diceroll_widget.configure(state='disabled')
-        output_widgets.append(diceroll_widget)
+            diceroll_widget = tk.Text(frame, font=word_font, height=1, width=len(word))
+            diceroll_widget.grid(row=2 + 2 * ((number_of_dicerolls - 1) // 5), column=(-1 + number_of_dicerolls) % 5)
+            diceroll_widget.configure(state='normal')
+            diceroll_widget.delete('1.0', 'end')
+            diceroll_widget.insert('1.0', str(diceroll))
+            diceroll_widget.configure(state='disabled')
+            output_widgets.append(diceroll_widget)
 
-        word_widget = tk.Text(frame, font=word_font, height=1, width=len(word))
-        word_widget.grid(row=3 + 2 * ((number_of_dicerolls - 1) // 5), column=(-1 + number_of_dicerolls) % 5)
-        word_widget.configure(state='normal')
-        word_widget.delete('1.0', 'end')
-        word_widget.insert('1.0', str(word))
-        word_widget.configure(state='disabled')
-        output_widgets.append(word_widget)
+            word_widget = tk.Text(frame, font=word_font, height=1, width=len(word))
+            word_widget.grid(row=3 + 2 * ((number_of_dicerolls - 1) // 5), column=(-1 + number_of_dicerolls) % 5)
+            word_widget.configure(state='normal')
+            word_widget.delete('1.0', 'end')
+            word_widget.insert('1.0', str(word))
+            word_widget.configure(state='disabled')
+            output_widgets.append(word_widget)
+        else:
+            print('Too many dicerolls')
