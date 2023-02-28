f9fc083aa7027364b1a3c8a182bd2a68db465150
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-02-28 16:21:26 +0200

    Update other_methods_gui.py

--- code/password_generation/other_methods_gui.py
+++ code/password_generation/other_methods_gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter.font import Font
 from password_generation import diceware_gui as diceware
 from password_generation import sentence_input_gui as sentence_input
 
@@ -10,6 +11,7 @@ def create_other_methods_window() -> None:
     Called upon app startup, this function prepares the other methods window for the user,
     by creating 2 frames: 1 for diceware, and 1 for sentence input.
     """
+    title_font = Font(family='Roboto', size=24)
 
     global other_methods_window
     if other_methods_window is not None and other_methods_window.winfo_exists():
@@ -21,10 +23,11 @@ def create_other_methods_window() -> None:
     other_methods_window.iconphoto(False, tk.PhotoImage(file='textures/logo.png'))
     other_methods_window.title(window_title)
 
-    diceware_frame = tk.LabelFrame(other_methods_window, text='Diceware', height=300, width=400)
+    diceware_frame = tk.LabelFrame(other_methods_window, text='Diceware', font=title_font, height=300, width=400)
     diceware_frame.grid(row=0, column=0)
 
-    sentence_input_frame = tk.LabelFrame(other_methods_window, text='Input a sentence', height=300, width=400)
+    sentence_input_frame = tk.LabelFrame(other_methods_window, text='Input a sentence', font=title_font,
+                                         height=300, width=400)
     sentence_input_frame.grid(row=0, column=1)
 
     diceware.create_diceware_frame(diceware_frame)
