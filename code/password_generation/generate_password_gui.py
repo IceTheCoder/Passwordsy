7c4496ee81b33ebd1fddbebd0e876282e790e249
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-03-04 19:29:40 +0200

    Update generate_password_gui.py

--- code/password_generation/generate_password_gui.py
+++ code/password_generation/generate_password_gui.py
@@ -114,7 +114,7 @@ def create_generate_password_frame(frame, done_btn_image) -> None:
         # Check if there is any content in all labels by checking the length (the length of an empty label is 1)
         if len(password_labels[0].get('1.0', 'end')) != 1 and len(password_labels[1].get('1.0', 'end')) != 1 and \
                 len(password_labels[2].get('1.0', 'end')) != 1 and len(password_labels[3].get('1.0', 'end')) != 1:
-            show_hide_all_slider.set()
+            show_hide_all_slider.set(1)
 
         button.configure(image=hide_btn_image, borderwidth=0, command=lambda: hide_password(index, button))
 
