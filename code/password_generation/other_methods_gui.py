e026007eb8c086fdf08b9835b8887be2266c1e75
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-03-05 18:08:52 +0200

    Attempted to change to Custom Tkinter

--- code/password_generation/other_methods_gui.py
+++ code/password_generation/other_methods_gui.py
@@ -3,39 +3,43 @@ This module prepares the other_methods_window for the user when they click the '
 """
 import tkinter as tk
 from tkinter.font import Font
+import customtkinter
+
 from password_generation import diceware_gui as diceware
 from password_generation import sentence_input_gui as sentence_input
 
 other_methods_window = None
 
 
-def create_other_methods_window() -> None:
+class OtherMethodsWindow(customtkinter.CTkToplevel):
     """
-    Called upon app startup, this function prepares the other methods window for the user,
-    by creating 2 frames: 1 for diceware, and 1 for sentence input.
+    This class contains the creation of the 'other methods' Toplevel window
     """
-    title_font = Font(family='Roboto', size=24)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        title_font = Font(family='Roboto', size=24)
 
-    global other_methods_window
-    if other_methods_window is not None and other_methods_window.winfo_exists():
-        return
+        if self is not None and self.winfo_exists():
+            return
 
-    other_methods_window = tk.Toplevel()
-    other_methods_window.geometry('1195x520')
-    window_title = 'Try other methods...'
+        self = tk.Toplevel()
+        self.geometry('1195x520')
+        window_title = 'Try other methods...'
 
-    other_methods_window.iconphoto(False, tk.PhotoImage(file='textures/logo.png'))
-    other_methods_window.title(window_title)
+        self.iconphoto(False, tk.PhotoImage(file='textures/logo.png'))
+        self.title(window_title)
 
-    diceware_frame = tk.LabelFrame(other_methods_window, text='Diceware', font=title_font)
-    diceware_frame.grid(row=0, column=0, sticky='nsew')
+        diceware_frame = tk.LabelFrame(self, text='Diceware', font=title_font)
+        diceware_frame.grid(row=0, column=0, sticky='nsew')
 
-    sentence_input_frame = tk.LabelFrame(other_methods_window, text='Input a sentence', font=title_font)
-    sentence_input_frame.grid(row=0, column=1, sticky='nsew')
+        sentence_input_frame = tk.LabelFrame(self, text='Input a sentence', font=title_font)
+        sentence_input_frame.grid(row=0, column=1, sticky='nsew')
 
-    diceware.create_diceware_frame(diceware_frame)
-    sentence_input.create_sentence_input_frame(sentence_input_frame)
+        diceware.create_diceware_frame(diceware_frame)
+        sentence_input.create_sentence_input_frame(sentence_input_frame)
 
-    other_methods_window.grid_rowconfigure(0, weight=1)
-    other_methods_window.grid_columnconfigure(0, weight=1)
-    other_methods_window.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+
+
