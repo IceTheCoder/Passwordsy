260a29f023a3083d67c57dcb387768e877112814
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-03-05 20:12:22 +0200

    still trying to implement that library, module, or whatever it is

--- code/password_generation/diceware_gui.py
+++ code/password_generation/diceware_gui.py
@@ -7,6 +7,7 @@ import tkinter as tk
 from tkinter import messagebox
 from tkinter.font import Font
 from PIL import ImageTk, Image
+import customtkinter
 
 import password_generation.diceware_logic as logic
 
@@ -16,102 +17,100 @@ global clear_btn_image
 global output_widgets
 
 
-def create_diceware_frame(frame):
+class DicewareFrame(customtkinter.CTkFrame):
     """
-    Called upon loading the pop-up window,
-    this function prepares the diceware frame for when the user decides to try other methods of password generations,
-    by setting up a Tkinter frame with a button, an entry box, and a text widget.
-
-    Parameters
-    ----------
-    frame: tk.Frame
-        The frame upon which the elements of dice ware shall be.
+    This class creates the diceware frame.
     """
-    frame.grid_rowconfigure(0, weight=1, uniform='row')
-    frame.grid_rowconfigure(1, weight=1, uniform='row')
-    frame.grid_rowconfigure(2, weight=1, uniform='row')
-    frame.grid_rowconfigure(3, weight=1, uniform='row')
-    frame.grid_rowconfigure(4, weight=1, uniform='row')
-    frame.grid_rowconfigure(5, weight=1, uniform='row')
-    frame.grid_rowconfigure(6, weight=1, uniform='row')
-    frame.grid_rowconfigure(7, weight=1, uniform='row')
-    frame.grid_rowconfigure(8, weight=1, uniform='row')
-    frame.grid_rowconfigure(9, weight=1, uniform='row')
-    frame.grid_rowconfigure(10, weight=1, uniform='row')
-    frame.grid_rowconfigure(11, weight=1, uniform='row')
-    frame.grid_rowconfigure(12, weight=1, uniform='row')
-    frame.grid_rowconfigure(13, weight=1, uniform='row')
-    frame.grid_rowconfigure(14, weight=1, uniform='row')
-    frame.grid_rowconfigure(15, weight=1, uniform='row')
-    frame.grid_columnconfigure(0, weight=1, uniform='column')
-    frame.grid_columnconfigure(1, weight=1, uniform='column')
-    frame.grid_columnconfigure(2, weight=1, uniform='column')
-    frame.grid_columnconfigure(3, weight=1, uniform='column')
-    frame.grid_columnconfigure(4, weight=1, uniform='column')
-    word_font = Font(family='Roboto', size=12)
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
 
-    global output_widgets
-    output_widgets = []
+        #self.grid_rowconfigure(0, weight=1, uniform='row')
+        #self.grid_rowconfigure(1, weight=1, uniform='row')
+        #self.grid_rowconfigure(2, weight=1, uniform='row')
+        #self.grid_rowconfigure(3, weight=1, uniform='row')
+        #self.grid_rowconfigure(4, weight=1, uniform='row')
+        #self.grid_rowconfigure(5, weight=1, uniform='row')
+        #self.grid_rowconfigure(6, weight=1, uniform='row')
+        #self.grid_rowconfigure(7, weight=1, uniform='row')
+        #self.grid_rowconfigure(8, weight=1, uniform='row')
+        #self.grid_rowconfigure(9, weight=1, uniform='row')
+        #self.grid_rowconfigure(10, weight=1, uniform='row')
+        #self.grid_rowconfigure(11, weight=1, uniform='row')
+        #self.grid_rowconfigure(12, weight=1, uniform='row')
+        #self.grid_rowconfigure(13, weight=1, uniform='row')
+        #self.grid_rowconfigure(14, weight=1, uniform='row')
+        #self.grid_rowconfigure(15, weight=1, uniform='row')
+        #self.grid_columnconfigure(0, weight=1, uniform='column')
+        #self.grid_columnconfigure(1, weight=1, uniform='column')
+        #self.grid_columnconfigure(2, weight=1, uniform='column')
+        #self.grid_columnconfigure(3, weight=1, uniform='column')
+        #self.grid_columnconfigure(4, weight=1, uniform='column')
+        self.word_font = Font(family='Roboto', size=12)
 
-    global number_of_dicerolls
-    number_of_dicerolls = 0
-
-    global roll_dice_btn_image
-    roll_dice_btn_image = ImageTk.PhotoImage(Image.open('textures/roll_dice_btn.png'))
-
-    roll_dice_button = tk.Button(frame, image=roll_dice_btn_image, borderwidth=0,
-                                 command=lambda: display_words(logic.roll_dice()))
-    roll_dice_button.grid(row=0, column=0, columnspan=5, pady=0, sticky='n')
-
-    def clear_frame():
-        """
-        This function does nothing.
-        """
         global output_widgets
-        global number_of_dicerolls
-
-        for widget in output_widgets:
-            widget.destroy()
-
         output_widgets = []
+
+        global number_of_dicerolls
         number_of_dicerolls = 0
 
-    global clear_btn_image
-    clear_btn_image = ImageTk.PhotoImage(Image.open('textures/clear_btn.png'))
-    clear_button = tk.Button(frame, image=clear_btn_image, borderwidth=0, command=clear_frame)
-    clear_button.grid(row=1, column=0, columnspan=5, pady=0, sticky='n')
+        global roll_dice_btn_image
+        roll_dice_btn_image = ImageTk.PhotoImage(Image.open('textures/roll_dice_btn.png'))
 
-    def display_words(pair):
-        """
-        Called when the user clicks the 'roll dice' button,
-        this function displays the pairs of dice rolls and words to the user.
+        self.roll_dice_button = tk.Button(self, image=roll_dice_btn_image, borderwidth=0,
+                                          command=lambda: display_words(logic.roll_dice()))
+        self.roll_dice_button.grid(row=0, column=0, columnspan=5, pady=0, sticky='n')
 
-        Parameters
-        ----------
-        pair: dict
-            Contains the pairs of dice roll numbers and related words according to the dice ware wordlist.
-        """
-        global number_of_dicerolls
-        if number_of_dicerolls < 35:
-            number_of_dicerolls += 1
-            (diceroll, word), = pair.items()
+        def clear_frame():
+            """
+            This function does nothing.
+            """
+            global output_widgets
+            global number_of_dicerolls
 
-            diceroll_widget = tk.Text(frame, font=word_font, height=1, width=len(word))
-            diceroll_widget.grid(row=2 + 2 * ((number_of_dicerolls - 1) // 5), column=(-1 + number_of_dicerolls) % 5)
-            diceroll_widget.configure(state='normal')
-            diceroll_widget.delete('1.0', 'end')
-            diceroll_widget.insert('1.0', str(diceroll))
-            diceroll_widget.configure(state='disabled')
-            output_widgets.append(diceroll_widget)
+            for widget in output_widgets:
+                widget.destroy()
 
-            word_widget = tk.Text(frame, font=word_font, height=1, width=len(word))
-            word_widget.grid(row=3 + 2 * ((number_of_dicerolls - 1) // 5), column=(-1 + number_of_dicerolls) % 5,
-                             sticky='n')
-            word_widget.configure(state='normal')
-            word_widget.delete('1.0', 'end')
-            word_widget.insert('1.0', str(word))
-            word_widget.configure(state='disabled')
-            output_widgets.append(word_widget)
-        else:
-            tk.messagebox.showwarning('Dice roll limit reached',
-                                      'You have reached the maximum limit of 35 dice rolls.')
+            output_widgets = []
+            self.number_of_dicerolls = 0
+
+        global clear_btn_image
+        clear_btn_image = ImageTk.PhotoImage(Image.open('textures/clear_btn.png'))
+        self.clear_button = tk.Button(self, image=clear_btn_image, borderwidth=0, command=clear_frame)
+        self.clear_button.grid(row=1, column=0, columnspan=5, pady=0, sticky='n')
+
+        def display_words(pair):
+            """
+            Called when the user clicks the 'roll dice' button,
+            this function displays the pairs of dice rolls and words to the user.
+
+            Parameters
+            ----------
+            pair: dict
+                Contains the pairs of dice roll numbers and related words according to the dice ware wordlist.
+            """
+            global number_of_dicerolls
+            if number_of_dicerolls < 35:
+                number_of_dicerolls += 1
+                (diceroll, word), = pair.items()
+
+                self.diceroll_widget = tk.Text(self, font=self.word_font, height=1, width=len(word))
+                self.diceroll_widget.grid(row=2 + 2 * ((number_of_dicerolls - 1) // 5),
+                                          column=(-1 + number_of_dicerolls) % 5)
+                self.diceroll_widget.configure(state='normal')
+                self.diceroll_widget.delete('1.0', 'end')
+                self.diceroll_widget.insert('1.0', str(diceroll))
+                self.diceroll_widget.configure(state='disabled')
+                output_widgets.append(diceroll_widget)
+
+                self.word_widget = tk.Text(self, font=self.word_font, height=1, width=len(word))
+                self.word_widget.grid(row=3 + 2 * ((number_of_dicerolls - 1) // 5),
+                                      column=(-1 + number_of_dicerolls) % 5,
+                                      sticky='n')
+                self.word_widget.configure(state='normal')
+                self.word_widget.delete('1.0', 'end')
+                self.word_widget.insert('1.0', str(word))
+                self.word_widget.configure(state='disabled')
+                output_widgets.append(word_widget)
+            else:
+                tk.messagebox.showwarning('Dice roll limit reached',
+                                          'You have reached the maximum limit of 35 dice rolls.')
