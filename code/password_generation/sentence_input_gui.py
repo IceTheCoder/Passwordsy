683f5e4206122c5d816f9b53e6503c342bab3829
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-03-11 14:41:41 +0200

    Update sentence_input_gui.py

--- code/password_generation/sentence_input_gui.py
+++ code/password_generation/sentence_input_gui.py
@@ -28,17 +28,17 @@ class SentenceInputToplevel(customtkinter.CTkToplevel):
         self.grid_rowconfigure(4, weight=1, uniform='row')
         self.grid_columnconfigure(0, weight=1, uniform='column')
 
-        self.instruction_font = customtkinter.CTkFont(family='Roboto', size=16)
-        self.warning_font = customtkinter.CTkFont(family='Roboto', size=14)
-        self.word_font = customtkinter.CTkFont(family='Roboto', size=12)
+        self.instruction_font = customtkinter.CTkFont(family='Roboto', size=24)
+        self.warning_font = customtkinter.CTkFont(family='Roboto', size=20)
+        self.word_font = customtkinter.CTkFont(family='Roboto', size=14)
 
         self.instruction_label = customtkinter.CTkLabel(master=self, text='Input a sentence', font=self.instruction_font)
         self.instruction_label.grid(row=0, column=0)
 
         self.input_box = customtkinter.CTkEntry(self, width=500, corner_radius=8.5)
-        self.input_box.grid(row=1, column=0, pady=(10, 5))
+        self.input_box.grid(row=1, column=0)
 
-        self.password_label = customtkinter.CTkTextbox(self, font=self.word_font, width=500, height=200)
+        self.password_label = customtkinter.CTkTextbox(self, font=self.word_font, width=500, height=50)
         self.password_label.grid(row=2, column=0)
 
         self.warning_label_1 = customtkinter.CTkLabel(master=self, font=self.warning_font)
@@ -85,7 +85,7 @@ class SentenceInputToplevel(customtkinter.CTkToplevel):
 
                 for index, warning in enumerate(logic.check_password_strength(None, password_output)):
                     self.warning_labels[index].configure(text=warning)
-                    self.warning_labels[index].grid(row=3 + index, column=0)
+                    self.warning_labels[index].grid(row=3 + index, column=0, padx=10)
             else:
                 for label in self.warning_labels:
                     label.configure(text='')
