79532ddc76a0b59656c969c8e7e3377e3557b84e
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-03-13 20:45:53 +0200

    Moved the show_copy_buton function to gui

--- code/password_generation/generate_password_logic.py
+++ code/password_generation/generate_password_logic.py
@@ -133,23 +133,6 @@ def generate_password(requested_password_length, lowercase_letters_var, uppercas
             return password
 
 
-def show_copy_button(event, copy) -> None:
-    """
-    Called when the user releases a mouse button on a password label,
-    this function uses the Tkinter module to display a contextual menu containing a 'copy' button
-    for copying the password to the clipboard on the x and y coordinates of the user's cursor,
-    where the y coordinates are adjusted by 30 pixels.
-
-    Parameters
-    ----------
-    event: tkinter.event
-        Gets the coordinates of the mouse cursor when the user releases a mouse button on a password_label.
-    copy: tkinter.Menu()
-        The copy button itself.
-    """
-    copy.tk_popup(event.x_root, event.y_root - 30)
-
-
 def copy_selected_text(input_box, labels) -> None:
     """
     Called upon pressing the copy button,
