ff751546df6cd2398df486dd4bc4b902121e7b43
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-03-15 19:38:40 +0200

    Update sentence_input_logic.py

--- code/password_generation/sentence_input_logic.py
+++ code/password_generation/sentence_input_logic.py
@@ -4,46 +4,6 @@ This module deals with the logic part of generating a secure password based on a
 import string
 
 
-def produce_password(sentence):
-    """
-    Called as the user types a sentence to be turned into a password,
-    this function takes the sentence the user is typing,
-    splits it into words,
-    and returns the first letter of each word,
-    as well as any punctuation and numbers.
-
-    Parameters
-    ----------
-    sentence: str
-        The sentence the user is typing.
-    """
-    if sentence != '':
-        password = ''
-
-        split_full_sentence = sentence.split(' ')
-        letters_only_sentence = ''
-
-        for word in sentence:
-            for letter in word:
-                if letter not in string.digits and letter not in string.punctuation:
-                    letters_only_sentence += letter
-
-        split_letters_only_sentence = letters_only_sentence.split(' ')
-
-        for word in split_full_sentence:
-            letter_taken = False
-            for character in word:
-                if character in string.digits or character in string.punctuation:
-                    password += character
-                elif not letter_taken:
-                    password += character
-                    letter_taken = True
-
-        return password
-    else:
-        return ''
-
-
 def check_password_strength(event, inputted_password) -> list | str:
     """
     Called upon pressing the done button,
