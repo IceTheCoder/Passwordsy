e68926031c998b4b684088a7e2a102f96cce0934
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-03-22 19:38:41 +0200

    Update generate_password_gui.py

--- code/password_generation/generate_password_gui.py
+++ code/password_generation/generate_password_gui.py
@@ -358,7 +358,8 @@ class PasswordGenerationFrame(customtkinter.CTkFrame):
                 Gets the coordinates of the mouse cursor when the user releases a mouse button on a password_label.
             """
             global copy_menu
-            copy_menu.tk_popup(event.x_root, event.y_root - 30) # https://youtu.be/Z4zePg2M5H8
+            # https://stackoverflow.com/questions/69425865/tkinter-event-x-y-mouse-position-wrong-value-only-when-mouse-movement-up
+            copy_menu.tk_popup(event.x_root, event.y_root - 30)  # https://youtu.be/Z4zePg2M5H8
 
             for label in self.password_labels:
                 label.tag_bind('copy_tag', '<Button-3>', show_copy_menu)
