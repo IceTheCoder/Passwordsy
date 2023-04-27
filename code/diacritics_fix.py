48ce63d8c1ad07a73653c960cec57aa57dc8bc3d
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-04-27 13:48:28 +0300

    it works

--- code/diacritics_fix.py
+++ code/diacritics_fix.py
@@ -1,10 +1,15 @@
 """
 ..
 """
+REMEDIED_CHARS = {ord(c): c for c in 'ȘșȚț'}
 
 
-def on_key_press():
+def on_key_press(e):
     """
     ...
     """
-    pass
+    char = REMEDIED_CHARS.get(e.keysym_num)
+    if char:
+        e.widget.insert('insert', e.char[:-1] + char)
+        return 'break'
+
