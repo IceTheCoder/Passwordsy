b935c0c58345b3cb02e7a773274ca65cab31ed07
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-05-22 15:00:36 +0300

    Update diacritics_fix.py

--- code/diacritics_fix.py
+++ code/diacritics_fix.py
@@ -6,12 +6,18 @@ import tkinter
 REMEDIED_CHARS = {ord(c): c for c in 'ĂăÂâÎîȘșTtȚțEeÉéÈèÊêËëIiÍíÌìÎîOoÓóÒòÔôÖöŐőUuÚúÙùÛûÜüŰűNnÑñÇçSsŚśŠšZzŽž'}
 
 
-def on_key_press(e: tkinter.Event):
+# https://stackoverflow.com/questions/75846986/certain-characters-like-%c8%9b-and-%c8%99-become-question-marks-as-i-type-them-in-a-tkin
+def on_key_press(e: tkinter.Event) -> str:
     """
-    ...
+    This function remedies a bug that made it impossible to type certain diacritics by finding those characters in the
+    input and replacing them properly.
+
+    Returns
+    -------
+    str
+        'break'.
     """
     char = REMEDIED_CHARS.get(e.keysym_num)
     if char:
         e.widget.insert('insert', e.char[:-1] + char)
         return 'break'
-
